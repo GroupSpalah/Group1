abac3843e1fa5cf87a4bb049e6f3920e66d30ab8
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2023-10-02 12:48:28 +0300

    Add  Animal yard 17_09_23

---
+++ src/homeworks/vladyslav_lazin/hw_2023/hw_17_09_23/Animal.java
@@ -0,0 +1,7 @@
+package homeworks.vladyslav_lazin.hw_2023.hw_17_09_23;
+
+public class Animal {
+    static int currentId = 0;
+    int id;
+    int weight;
+}
---
+++ src/homeworks/vladyslav_lazin/hw_2023/hw_17_09_23/Dog.java
@@ -0,0 +1,2 @@
+package homeworks.vladyslav_lazin.hw_2023.hw_17_09_23;public class Dog {
+}
