0b88a73bf3d5680b01f641f080fbdf53312fe315
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-01-07 17:14:25 +0200

    hw_24_12_23 resolved

---
+++ src/homeworks/vladyslav_lazin/hw_2023/hw_24_12_23/template_method/Cross.java
@@ -0,0 +1,14 @@
+package homeworks.vladyslav_lazin.hw_2023.hw_24_12_23.template_method;
+
+public class Cross extends Figures{
+
+    @Override
+    public void showAnotherFigure() {
+        System.out.println("   ***\n" +
+                "   ***\n" +
+                "**********\n" +
+                "**********\n" +
+                "   ***\n" +
+                "   ***\n");
+    }
+}
---
+++ src/homeworks/vladyslav_lazin/hw_2023/hw_24_12_23/template_method/Line.java
@@ -0,0 +1,8 @@
+package homeworks.vladyslav_lazin.hw_2023.hw_24_12_23.template_method;
+
+public class Line extends Figures{
+    @Override
+    public void showAnotherFigure() {
+        System.out.println("*********\n");
+    }
+}
