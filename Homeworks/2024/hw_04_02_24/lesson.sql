b030d44c8de6fb58b93e4b4b66991603651e5170
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-02-04 20:31:42 +0200

    Add lesson 04_02_24

---
+++ Homeworks/2024/hw_04_02_24/lesson.sql
@@ -0,0 +1,11 @@
+ALTER TABLE table_name
+    ADD COLUMN column_name VARCHAR(30);
+
+ALTER TABLE table_name
+DROP COLUMN column_name;
+
+ALTER TABLE table_name
+CHANGE COLUMN column_name new_name VARCHAR(30);
+
+ALTER TABLE table_name
+MODIFY COLUMN column_name VARCHAR(100);
--- src/homeworks/vladyslav_lazin/hw_2024/hw_28_01_24/online_shop_script.sql
+++ src/homeworks/vladyslav_lazin/hw_2024/hw_28_01_24/online_shop_script.sql
@@ -2,28 +2,28 @@ CREATE DATABASE online_shop;
 USE online_shop;
 CREATE TABLE products
 (
-    product_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    product_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     description TEXT,
     price DECIMAL(8,2)
 );
 CREATE TABLE produsct_photos
 (
-    product_photo_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    product_photo_id INT AUTO_INCREMENT PRIMARY KEY,
     url VARCHAR(255),
     product_id INT,
     FOREIGN KEY(product_id) REFERENCES products(product_id)
 );
 CREATE TABLE customers
 (
-    customer_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     phone VARCHAR(20),
     email VARCHAR(40)
 );
 CREATE TABLE carts
 (
-    cart_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    cart_id INT AUTO_INCREMENT PRIMARY KEY,
     customer_id INT,
     FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
 );
