4890996ed2923304b894761f2efe97fc0f669df7
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-02-04 18:15:38 +0200

    add sql script to task 28_01_24

---
+++ src/homeworks/vladyslav_lazin/hw_2024/hw_28_01_24/online_shop_script.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE online_shop;
+USE online_shop;
+CREATE TABLE products
+(
+    product_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    description TEXT,
+    price DECIMAL(8,2)
+);
+CREATE TABLE produsct_photos
+(
+    product_photo_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    url VARCHAR(255),
+    product_id INT,
+    FOREIGN KEY(product_id) REFERENCES products(product_id)
+);
+CREATE TABLE customers
+(
+    customer_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(20),
+    email VARCHAR(40)
+);
+CREATE TABLE carts
+(
+    cart_id INT AUTO_INCREMENT PRIMARY KEY NOT NULL,
+    customer_id INT,
+    FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+);
+CREATE TABLE carts_to_products
+(
+    cart_id INT,
+    product_id INT,
+    FOREIGN KEY(cart_id) REFERENCES carts(cart_id),
+    FOREIGN KEY(product_id) REFERENCES products(product_id)
+);
