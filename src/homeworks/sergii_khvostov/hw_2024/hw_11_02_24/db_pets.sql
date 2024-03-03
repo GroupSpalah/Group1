d4fcea11a6d1683340360cc708a38dacb7592cd6
Sergii Khvostov <yasha3ua@gmail.com>
2024-03-03 13:07:55 +0200

    Pets,Notebooks 25.02.24

--- src/homeworks/sergii_khvostov/hw_2024/hw_11_02_24/db_pets.sql
+++ src/homeworks/sergii_khvostov/hw_2024/hw_11_02_24/db_pets.sql
@@ -6,8 +6,8 @@ id int AUTO_INCREMENT PRIMARY KEY,
 typo varchar(20)
 );
 
-INSERT INTO breeds (typo)
-VALUES
+insert into breeds (typo)
+values
 ('collie'),
 ('bulldog'),
 ('poodle'),
@@ -22,8 +22,8 @@ id int AUTO_INCREMENT PRIMARY KEY,
 name varchar(20)
 );
 
-INSERT INTO masters (name)
-VALUES
+insert into masters (name)
+values
 ('Tintin'),
 ('Willian'),
 ('Fedor'),
@@ -33,18 +33,17 @@ VALUES
 ('Richard'),
 ('Gregory');
 
-
 create table dogs(
 id int AUTO_INCREMENT PRIMARY KEY,
 name varchar(20),
 breeds_id int,
 FOREIGN KEY (breeds_id) REFERENCES breeds(id),
 masters_id int,
-FOREIGN KEY (masters_id) REFERENCES masters(id) ON DELETE CASCADE
+FOREIGN KEY (masters_id) REFERENCES masters(id) ON delete CASCADE
 );
 
-INSERT INTO dogs (name, breeds_id, masters_id)
-VALUES
+insert into dogs (name, breeds_id, masters_id)
+values
 ('sharik', 4, 3),
 ('artemon', 3, 5),
 ('rex', 8, 7),
@@ -54,11 +53,10 @@ VALUES
 ('hachico', 7, 4),
 ('spike', 2, 2);
 
-
-SELECT dogs.id AS dogs_id, dogs.name AS dogs_name, breeds.typo AS breeds_type, masters.name AS masters_name
-FROM dogs
-INNER JOIN breeds ON dogs.breeds_id  = breeds.id
-INNER JOIN masters ON dogs.masters_id = masters.id
+select dogs.id as dogs_id, dogs.name as dogs_name, breeds.typo as breeds_type, masters.name as masters_name
+from dogs
+inner join breeds on dogs.breeds_id  = breeds.id
+inner join masters on dogs.masters_id = masters.id
 
 
 
