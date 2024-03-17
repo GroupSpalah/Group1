2a63976da3c31334018a731fa562af7c39ddf1ed
Dmytro Koliesnik <kodi9693@gmail.com>
2024-03-17 12:48:26 +0200

    Learn records, streams

--- src/homeworks/dmytro_k/hw_2024/hw_10_03_24/TestThread1.java
+++ src/homeworks/dmytro_k/hw_2024/hw_10_03_24/TestThread1.java
@@ -1,7 +1,5 @@
 package homeworks.dmytro_k.hw_2024.hw_10_03_24;
 
-import java.lang.management.ManagementFactory;
-
 public class TestThread1 extends Thread {
     @Override
     public void run() {
