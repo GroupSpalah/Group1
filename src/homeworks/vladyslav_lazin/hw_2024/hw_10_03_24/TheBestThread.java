d064e652bd7d6265e6ff62fddc8172dd43419b01
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-03-20 12:01:50 +0200

    fix some issues 3 hw_10_03_24

--- src/homeworks/vladyslav_lazin/hw_2024/hw_10_03_24/TheBestThread.java
+++ src/homeworks/vladyslav_lazin/hw_2024/hw_10_03_24/TheBestThread.java
@@ -2,7 +2,6 @@ package homeworks.vladyslav_lazin.hw_2024.hw_10_03_24;
 
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.Arrays;
 import java.util.List;
@@ -10,54 +9,43 @@ import java.util.List;
 public class TheBestThread {
     public static void main(String[] args) {
         List<String> words = List.of("we", "i", "holidays", "house", "can");
-
-         words.forEach(word -> new Thread(new MyThread(word)));
-
+        words.forEach(word -> new Thread(new MyThread(word)));
     }
+}
 
+class MyThread implements Runnable {
+    private final String countedWord;
 
-    public static String readFile() {
+    public MyThread(String countedWord) {
+        this.countedWord = countedWord;
 
-        Path pathToFile = Paths.get("./src/homeworks/vladyslav_lazin/hw_2024/hw_10_03_24/Story.txt");
-
-        String line = null;
+        Thread thread = new Thread(this);
+        thread.start();
         try {
-            line = Files.readString(pathToFile);
-        } catch (IOException e) {
+            thread.join();
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        return line;
     }
 
-    public static long wordCount(String countedWord, String line) {
+    private static long wordCount(String countedWord, String line) {
         String[] words = line.split("\\s+");
         return Arrays.stream(words)
                 .map(String::toLowerCase)
                 .filter(word -> word.equals(countedWord))
                 .count();
     }
-}
-
-class MyThread implements Runnable {
-    private final String countedWord;
-        
-    public MyThread(String countedWord) {
-        this.countedWord = countedWord;
-
-        Thread t = new Thread(this);
-        t.start();
-        try {
-            t.join();
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-    }
 
     public void run() {
         long startTime = System.currentTimeMillis();
-        String line = TheBestThread.readFile();
+        String line;
+        try {
+            line = Files.readString(Paths.get("./src/homeworks/vladyslav_lazin/hw_2024/hw_10_03_24/Story.txt"));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
 
-        long count = TheBestThread.wordCount(countedWord, line);
+        long count = wordCount(countedWord, line);
         long time = System.currentTimeMillis() - startTime;
 
         System.out.printf("Counted word: %s   count: %s times: %s ms%n", countedWord, count, time);
---
+++ src/homeworks/vladyslav_lazin/hw_2024/hw_17_03_24/Task1.java
@@ -0,0 +1,5 @@
+package homeworks.vladyslav_lazin.hw_2024.hw_17_03_24;
+
+public class Task1 {
+
+}
