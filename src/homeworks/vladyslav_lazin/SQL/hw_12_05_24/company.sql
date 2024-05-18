ca86e8ab570edcdde6de1eef3510f9c466599efa
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-05-18 09:31:15 +0300

    progres in hw_12_05_24

---
+++ src/homeworks/vladyslav_lazin/SQL/hw_12_05_24/company.sql
@@ -0,0 +1,53 @@
+CREATE DATABASE company;
+USE company;
+CREATE TABLE depts(
+	dept_id INT AUTO_INCREMENT PRIMARY KEY,
+	city VARCHAR(25)
+);
+
+CREATE TABLE empls(
+	empl_id INT AUTO_INCREMENT PRIMARY KEY,
+	first_name VARCHAR(25),
+	last_name VARCHAR(25),
+	fk_dept_id INT,
+	FOREIGN KEY (fk_dept_id) REFERENCES depts(dept_id)
+);
+
+--1.
+SELECT city
+FROM depts d;
+
+--2.
+SELECT DISTINCT last_name, first_name
+FROM empls
+ORDER BY last_name;
+
+--3.
+SELECT COUNT(*)
+FROM empls
+WHERE fk_dept_id = 3;
+
+--4.
+SELECT fk_dept_id, COUNT(*) count
+FROM empls
+GROUP BY fk_dept_id
+ORDER BY count DESC;
+
+--5.
+SELECT fk_dept_id, COUNT(*) count
+FROM empls
+GROUP BY fk_dept_id
+ORDER BY fk_dept_id;
+
+--6.
+SELECT city
+FROM depts
+WHERE city LIKE 'S%';
+
+INSERT INTO depts (city) VALUES
+('New York'), ('Los Angeles'), ('Chicago'), ('Houston'), ('Phoenix'),
+('Philadelphia'), ('San Antonio'), ('San Diego'), ('Dallas'), ('San Jose');
+
+INSERT INTO empls (first_name, last_name, fk_dept_id)
+VALUES
+('John', 'Doe', 3);
--- src/homeworks/vladyslav_lazin/SQL/hw_12_05_24/student_registry/create_sheme.sql
+++ src/homeworks/vladyslav_lazin/SQL/hw_12_05_24/student_registry/create_sheme.sql
@@ -9,11 +9,50 @@ CREATE TABLE addresses (
 );
 CREATE TABLE faculties (
 	faculty_id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(30),
+	name VARCHAR(30) NOT NULL,
 	found_date DATE
 );
-CREATE TABLE universities (
-	university_id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(30),
+CREATE TABLE univercities (
+	univercity_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30) NOT NULL,
 	found_date DATE
 );
+
+CREATE TABLE faculty_to_university (
+	fk_faculty_id INT NOT NULL,
+	fk_univercity_id INT NOT NULL,
+	FOREIGN KEY(fk_faculty_id)
+	REFERENCES faculties(faculty_id),
+	FOREIGN KEY(fk_univercity_id)
+	REFERENCES univercities(univercity_id)
+);
+
+
+CREATE TABLE students (
+	student_id INT AUTO_INCREMENT PRIMARY KEY,
+	name VARCHAR(30) NOT NULL,
+	surname VARCHAR(30) NOT NULL,
+	gender ENUM('male', 'female', 'smth else') NOT NULL,
+	birthday DATE NOT NULL,
+	fk_st_group_id INT NOT NULL
+);
+
+CREATE TABLE st_groups(
+	st_group_id INT AUTO_INCREMENT PRIMARY KEY,
+	found_date DATE,
+	group_leader INT,
+	fk_faculty_id INT
+);
+
+ALTER TABLE students
+ADD CONSTRAINT
+FOREIGN KEY (fk_st_group_id)
+REFERENCES st_groups(st_group_id);
+
+ALTER TABLE st_groups
+ADD CONSTRAINT
+FOREIGN KEY (group_leader)
+REFERENCES students(student_id),
+ADD CONSTRAINT
+FOREIGN KEY (fk_faculty_id)
+REFERENCES faculties(faculty_id);
