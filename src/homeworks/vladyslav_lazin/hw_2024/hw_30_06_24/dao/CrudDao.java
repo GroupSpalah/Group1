33a3d5a973395c7fb21b049c39c4d377d4be888c
Vladyslav Lazin <wladislaw.lazin@gmail.com>
2024-07-20 11:09:26 +0300

    added findall and deletebyid to hw_30_06_24

--- src/homeworks/vladyslav_lazin/hw_2024/hw_30_06_24/dao/CrudDao.java
+++ src/homeworks/vladyslav_lazin/hw_2024/hw_30_06_24/dao/CrudDao.java
@@ -1,7 +1,13 @@
 package homeworks.vladyslav_lazin.hw_2024.hw_30_06_24.dao;
 
+import java.util.List;
+
 public interface CrudDao<T> {
     void save(T t);
 
     T findById(int id);
+
+    List<T> findAll();
+
+    void deleteById(int id);
 }
